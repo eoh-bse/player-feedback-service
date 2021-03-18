64f16e6035a8a529ccce9c472fdd7dcc4b55a161
Elbert Oh <oh.applications@gmail.com>
2021-03-18 01:08:47 -0400

    small cleanup

--- src/PlayerFeedbackService.Infrastructure/DependencyInjection/Kafka.cs
+++ src/PlayerFeedbackService.Infrastructure/DependencyInjection/Kafka.cs
@@ -43,7 +43,6 @@ namespace PlayerFeedbackService.Infrastructure.DependencyInjection
             var adminClientConfig = new AdminClientConfig
             {
                 BootstrapServers = brokerConfig.Servers,
-
             };
 
             using var adminClient = new AdminClientBuilder(adminClientConfig).Build();
